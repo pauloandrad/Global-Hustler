3361cf5424a23be20ee15a1113dce576c863e7aa
paulo andrade <pauloh8755@outlook.com.br>
2025-03-20 21:31:50 -0300

    first commit

---
+++ src/components/Earth/index.tsx
@@ -0,0 +1,88 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import createGlobe from "cobe";
+import { cn } from "@/lib/utils";
+
+interface EarthProps {
+  className?: string;
+  theta?: number;
+  dark?: number;
+  scale?: number;
+  diffuse?: number;
+  mapSamples?: number;
+  mapBrightness?: number;
+  baseColor?: [number, number, number];
+  markerColor?: [number, number, number];
+  glowColor?: [number, number, number];
+}
+const Earth: React.FC<EarthProps> = ({
+  className,
+  theta = 0.25,
+  dark = 1,
+  scale = 1.1,
+  diffuse = 1.2,
+  mapSamples = 40000,
+  mapBrightness = 6,
+  baseColor = [0.2509, 0.2509, 0.2509],
+  markerColor = [0.8314, 0.6392, 0.1922],
+  glowColor = [0.6902, 0.6902, 0.6902],
+}) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    let width = 0;
+    const onResize = () =>
+      canvasRef.current && (width = canvasRef.current.offsetWidth);
+    window.addEventListener("resize", onResize);
+    onResize();
+    let phi = 0;
+
+    onResize();
+    const globe = createGlobe(canvasRef.current!, {
+      devicePixelRatio: 2,
+      width: width * 2,
+      height: width * 2,
+      phi: 0,
+      theta: theta,
+      dark: dark,
+      scale: scale,
+      diffuse: diffuse,
+      mapSamples: mapSamples,
+      mapBrightness: mapBrightness,
+      baseColor: baseColor,
+      markerColor: markerColor,
+      glowColor: glowColor,
+      opacity: 1,
+      offset: [0, 0],
+      markers: [
+        { location: [-15.7801, -47.9292], size: 0.1 },
+        { location: [-0.1807, -78.4678], size: 0.1 },
+        { location: [4.7110, -74.0721], size: 0.2},
+      ],
+      onRender: (state: Record<string, any>) => {
+        state.phi = phi;
+        phi += 0.003;
+      },
+    });
+
+    return () => {
+      globe.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        style={{
+          width: "100%",
+          height: "100%",
+          maxWidth: "100%",
+          aspectRatio: "1",
+        }}
+      />
+    </div>
+  );
+};
+
+export default Earth;
